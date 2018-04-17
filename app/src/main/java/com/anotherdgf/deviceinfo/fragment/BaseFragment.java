fa44f6f0d37fd9c8d047bcf9bb6a13baa10b12ac
denggaofeng <1415356791@qq.com>
2018-04-17 22:55:02 +0800

    添加fragment类

---
+++ app/src/main/java/com/anotherdgf/deviceinfo/fragment/BaseFragment.java
@@ -0,0 +1,8 @@
+package com.anotherdgf.deviceinfo.fragment;
+
+/**
+ * Created by DGF on 2018/4/17.
+ */
+
+public class BaseFragment {
+}
---
+++ app/src/main/java/com/anotherdgf/deviceinfo/fragment/SettingsFragment.java
@@ -0,0 +1,11 @@
+package com.anotherdgf.deviceinfo.fragment;
+
+/**
+ * Created by DGF on 2018/4/17.
+ */
+
+public class SettingsFragment {
+    public SettingsFragment() {
+        super();
+    }
+}
