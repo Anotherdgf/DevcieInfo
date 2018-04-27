5bd46465cb42b1e9d64a20846300719c16c7d7e7
denggaofeng <denggaofeng@begoit.com>
2018-04-27 15:07:53 +0800

    修改设置界面statusbar浸入

--- app/src/main/java/com/anotherdgf/deviceinfo/activity/BaseActivity.java
+++ app/src/main/java/com/anotherdgf/deviceinfo/activity/BaseActivity.java
@@ -1,9 +1,12 @@
 package com.anotherdgf.deviceinfo.activity;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
 
 import com.anotherdgf.deviceinfo.fragment.BaseFragment;
+import com.gyf.barlibrary.ImmersionBar;
 
 /**
  * Created by denggaofeng on 2018/4/18.
@@ -11,11 +14,27 @@ import com.anotherdgf.deviceinfo.fragment.BaseFragment;
 
 public abstract class BaseActivity extends AppCompatActivity{
 
+    private ImmersionBar mImmersionBar;
     //获取布局文件ID
     protected abstract int getContentViewId();
 
     //布局中Fragment的ID
     protected abstract int getFragmentContentId();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mImmersionBar = ImmersionBar.with(this);
+        mImmersionBar.init();   //所有子类都将继承这些相同的属性
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mImmersionBar != null)
+            mImmersionBar.destroy();  //必须调用该方法，防止内存泄漏，不调用该方法，如果界面bar发生改变，在不关闭app的情况下，退出此界面再进入将记忆最后一次bar改变的状态
+    }
 
     //添加Fragment
     public void addFragment(BaseFragment fragment){
--- app/src/main/java/com/anotherdgf/deviceinfo/activity/SettingsActivity.java
+++ app/src/main/java/com/anotherdgf/deviceinfo/activity/SettingsActivity.java
@@ -3,13 +3,17 @@ package com.anotherdgf.deviceinfo.activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.anotherdgf.deviceinfo.R;
 import com.anotherdgf.deviceinfo.fragment.SettingsFragment;
+import com.gyf.barlibrary.ImmersionBar;
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends BaseActivity{
 
     private Toolbar sToolbar;
+    protected ImmersionBar mImmersionBar;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -17,13 +21,20 @@ public class SettingsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_settings);
 
         initView();
-
+        setStatusBar();
     }
 
     private void initView(){
         sToolbar = findViewById(R.id.toolbar_settings);
         setSupportActionBar(sToolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        sToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
         getFragmentManager()
                 .beginTransaction()
@@ -32,9 +43,26 @@ public class SettingsActivity extends AppCompatActivity {
 
     }
 
+    protected void setStatusBar() {
+        mImmersionBar = ImmersionBar.with(this);
+        mImmersionBar.statusBarColor(R.color.colorPrimaryDark)
+                .fitsSystemWindows(true)
+                .init();
+    }
+
     @Override
     public void onBackPressed() {
         finish();
+    }
+
+    @Override
+    protected int getContentViewId(){
+        return R.layout.activity_settings;
+    }
+
+    @Override
+    protected int getFragmentContentId(){
+        return R.id.fragment_container;
     }
 
 }
