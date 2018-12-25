038be814ec3e51c0cdebb9737f1dbedd31bc2ea5
denggaofeng <1415356791@qq.com>
2018-12-25 19:00:29 +0800

    add app update module

---
+++ appupdate/src/main/java/com/another/appupdate/base/BaseHttpDownloadManager.java
@@ -0,0 +1,21 @@
+package com.another.appupdate.base;
+
+
+import com.another.appupdate.listener.OnDownloadListener;
+
+public abstract class BaseHttpDownloadManager {
+
+    /**
+     * 下载apk
+     *
+     * @param apkUrl   apk下载地址
+     * @param apkName  apk名字
+     * @param listener 回调
+     */
+    public abstract void download(String apkUrl, String apkName, OnDownloadListener listener);
+
+    /**
+     * 取消下载apk
+     */
+    public abstract void cancel();
+}
